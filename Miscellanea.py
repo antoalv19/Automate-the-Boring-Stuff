c9b0ba350e31ba636734053413ffd3d03ead443b
Antonio <antonioalvino19@gmail.com>
2019-11-14 18:39:21 +0100

    Miscellanea tests

---
+++ Miscellanea.py
@@ -0,0 +1,28 @@
+# one infinite loop
+"""while True:
+    print("Chi sei?")
+    name = input()
+    if name != "Anto":
+        continue
+    print("Ciao Anto quale è la password?")
+    password = input()
+    if password == "Wido":
+        break
+print("Accesso Consenito")"""
+
+# for loops
+"""print("Il mio nome è")
+for i in range(6):
+    print("Anto sei volte", "(", str(i), ")")"""
+
+# print 10 for loop
+"""
+for i in range(1, 11):
+    print(i)
+"""
+# print 10 while loop
+a = 1
+while a <= 10:
+    print(a)
+    a = a + 1
+
